d3f92c9ef2fc261d2b9ee81f3bfa9fb83b650b5c
Nikolai Kochergin <Kocherginizer@gmail.com>
2024-04-03 14:26:55 +0300

    Add server game over logic

--- src/rooms/02-state-handler.ts
+++ src/rooms/02-state-handler.ts
@@ -20,6 +20,7 @@ export class State extends Schema {
     @type([Vector2Float]) apples = new ArraySchema<Vector2Float>();
 
     appleLastId = 0;
+    gameOverIDs = [];
 
     createApple(){
         const apple = new Vector2Float();
@@ -45,12 +46,40 @@ export class State extends Schema {
     }
 
     removePlayer(sessionId: string) {
-        this.players.delete(sessionId);
+        if(this.players.has(sessionId))
+            this.players.delete(sessionId);
     }
 
     movePlayer (sessionId: string, movement: any) {
         this.players.get(sessionId).x = movement.x;
         this.players.get(sessionId).z = movement.z;
+    }
+
+    gameOver(data){
+        const detailsPositions = JSON.parse(data);
+        const clientID = detailsPositions.id;
+
+        const gameOverID = this.gameOverIDs.find(value => value === clientID);
+        if(gameOverID !== undefined) return;
+        this.gameOverIDs.push(clientID);
+        this.delayClearGameOverIDs(clientID);
+
+        this.removePlayer(clientID);
+
+        for (let i = 0; i < detailsPositions.ds.length; i++) {
+            const apple = new Vector2Float();
+            apple.id = this.appleLastId++;
+            apple.x = detailsPositions.ds[i].x;
+            apple.z = detailsPositions.ds[i].z;
+            this.apples.push(apple);
+        }
+    }
+
+    async delayClearGameOverIDs(clientID){
+        await new Promise(resolve => setTimeout(resolve, 10000));
+        const index = this.gameOverIDs.findIndex(value => value === clientID);
+        if(index <= -1) return;
+        this.gameOverIDs.splice(index, 1);
     }
 }
 
@@ -68,6 +97,10 @@ export class StateHandlerRoom extends Room<State> {
         this.onMessage("collect", (client, data) => {
             const player = this.state.players.get(client.sessionId);
             this.state.collectApple(player, data);
+        });
+
+        this.onMessage("gameOver", (client, data) => {
+            this.state.gameOver(data);
         });
 
         for (let i = 0; i < this.startAppleCount; i++) {
